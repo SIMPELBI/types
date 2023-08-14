ba6a47d62058fa615b1069656b731a53aeba9e83
JPratama7 <josepratama080@gmail.com>
2023-08-14 15:10:33 +0700

    metode get mengembalikan slice dari schema

--- interface.go
+++ interface.go
@@ -4,7 +4,7 @@ import "context"
 
 type Schemas[I any, O any] interface {
 	GetById(context.Context, I) (O, error)
-	Get(context.Context) (O, error)
+	Get(context.Context) ([]O, error)
 	Update(context.Context, I) error
 	Delete(context.Context, I) error
 }
