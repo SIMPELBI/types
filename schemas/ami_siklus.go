9bcc0e1ce20a79d537a097fd6956084b370dea6f
Indra Riksa Herlambang <indrariksa@gmail.com>
2023-08-16 11:36:08 +0700

    update struct siklus

--- schemas/ami_siklus.go
+++ schemas/ami_siklus.go
@@ -4,7 +4,7 @@ import "github.com/golang-module/carbon/v2"
 
 type AmiSiklus struct {
 	IDSiklus int           `gorm:"primaryKey;column:id_siklus" json:"-"`
-	Tahun    carbon.Carbon `gorm:"column:tahun" json:"tahun"`
+	Tahun    int `gorm:"column:tahun" json:"tahun"`
 }
 
 func (m *AmiSiklus) TableName() string {
