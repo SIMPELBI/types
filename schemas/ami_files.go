ad8bfa18b76922842d37c4b0447152d3a0f4ea21
Valen Rionald <vrionald@gmail.com>
2023-09-12 17:21:54 +0700

    Tambah AmiFilesJoin untuk Get data join

--- schemas/ami_files.go
+++ schemas/ami_files.go
@@ -3,6 +3,15 @@ package model
 import "github.com/golang-module/carbon/v2"
 
 type AmiFiles struct {
+	IDFile   int           `gorm:"primaryKey;column:id_file" json:"-"`
+	IDSiklus int           `gorm:"column:id_siklus" json:"id_siklus"`
+	Judul    string        `gorm:"column:judul" json:"judul"`
+	File     string        `gorm:"column:file" json:"file"`
+	Tgl      carbon.Carbon `gorm:"column:tgl" json:"tgl"`
+	IDAdmin  int           `gorm:"column:id_admin" json:"id_admin"`
+}
+
+type AmiFilesJoin struct {
 	IDFile    int           `gorm:"primaryKey;column:id_file" json:"-"`
 	IDSiklus  int           `gorm:"column:id_siklus" json:"id_siklus"`
 	Tahun     string        `gorm:"column:tahun" json:"tahun"`
