886d30624ff88594022ce1acfd0a0adb651bb874
Valen Rionald <vrionald@gmail.com>
2023-09-12 11:04:51 +0700

    Tambah struct kebutuhan Join di AmiFilesProdi

--- schemas/ami_files_prodi.go
+++ schemas/ami_files_prodi.go
@@ -3,12 +3,14 @@ package model
 import "github.com/golang-module/carbon/v2"
 
 type AmiFilesProdi struct {
-	IDFile   int           `gorm:"primaryKey;column:id_file" json:"-"`
-	IDSiklus int           `gorm:"column:id_siklus" json:"id_siklus"`
-	Judul    string        `gorm:"column:judul" json:"judul"`
-	File     string        `gorm:"column:file" json:"file"`
-	Tgl      carbon.Carbon `gorm:"column:tgl" json:"tgl"`
-	IDAdmin  int           `gorm:"column:id_admin" json:"id_admin"`
+	IDFile    int           `gorm:"primaryKey;column:id_file" json:"-"`
+	IDSiklus  int           `gorm:"column:id_siklus" json:"id_siklus"`
+	Tahun     string        `gorm:"column:tahun" json:"tahun"`
+	Judul     string        `gorm:"column:judul" json:"judul"`
+	File      string        `gorm:"column:file" json:"file"`
+	Tgl       carbon.Carbon `gorm:"column:tgl" json:"tgl"`
+	IDAdmin   int           `gorm:"column:id_admin" json:"id_admin"`
+	NamaAdmin string        `gorm:"column:nm_admin" json:"nm_admin"`
 }
 
 func (m *AmiFilesProdi) TableName() string {
