06bcf81fd966b2dad0335c5e6e7dbc4cf2f3a357
Valen Rionald <vrionald@gmail.com>
2023-09-14 11:11:22 +0700

    Memperbaiki struct ami files prodi

--- schemas/ami_files_prodi.go
+++ schemas/ami_files_prodi.go
@@ -3,6 +3,15 @@ package model
 import "github.com/golang-module/carbon/v2"
 
 type AmiFilesProdi struct {
+	IDFile   int           `gorm:"primaryKey;column:id_file" json:"-"`
+	IDSiklus int           `gorm:"column:id_siklus" json:"id_siklus"`
+	Judul    string        `gorm:"column:judul" json:"judul"`
+	File     string        `gorm:"column:file" json:"file"`
+	Tgl      carbon.Carbon `gorm:"column:tgl" json:"tgl"`
+	IDAdmin  int           `gorm:"column:id_admin" json:"id_admin"`
+}
+
+type AmiFilesProdiJoin struct {
 	IDFile    int           `gorm:"primaryKey;column:id_file" json:"-"`
 	IDSiklus  int           `gorm:"column:id_siklus" json:"id_siklus"`
 	Tahun     string        `gorm:"column:tahun" json:"tahun"`
