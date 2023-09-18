8920cd764227bd3144ec96fdd40ec12b80c852b2
valenrio66 <bachtiarramadhan21>
2023-09-18 14:57:22 +0700

    Ganti tipe data Tgl di AmiMekanisme

--- schemas/ami_mekanisme.go
+++ schemas/ami_mekanisme.go
@@ -1,20 +1,18 @@
 package model
 
-import (
-	"database/sql"
-)
+import "github.com/golang-module/carbon/v2"
 
 type AmiMekanisme struct {
-	IDMekanisme int          `gorm:"primaryKey;column:id_mekanisme" json:"-"`
-	IDAmi       int          `gorm:"column:id_ami" json:"idAmi"`
-	Question1   string       `gorm:"column:question1" json:"question1"`
-	Question2   string       `gorm:"column:question2" json:"question2"`
-	Question3   string       `gorm:"column:question3" json:"question3"`
-	Question4   string       `gorm:"column:question4" json:"question4"`
-	Question5   string       `gorm:"column:question5" json:"question5"`
-	Question6   string       `gorm:"column:question6" json:"question6"`
-	Tgl         sql.NullTime `gorm:"column:tgl" json:"tgl"`
-	IDAuditor   int          `gorm:"column:id_auditor" json:"idAuditor"`
+	IDMekanisme int           `gorm:"primaryKey;column:id_mekanisme" json:"-"`
+	IDAmi       int           `gorm:"column:id_ami" json:"idAmi"`
+	Question1   string        `gorm:"column:question1" json:"question1"`
+	Question2   string        `gorm:"column:question2" json:"question2"`
+	Question3   string        `gorm:"column:question3" json:"question3"`
+	Question4   string        `gorm:"column:question4" json:"question4"`
+	Question5   string        `gorm:"column:question5" json:"question5"`
+	Question6   string        `gorm:"column:question6" json:"question6"`
+	Tgl         carbon.Carbon `gorm:"column:tgl" json:"tgl"`
+	IDAuditor   int           `gorm:"column:id_auditor" json:"idAuditor"`
 }
 
 func (m *AmiMekanisme) TableName() string {
