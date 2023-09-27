91ee88d4d58beec7598d73ef83c635bf40d766d2
Valen Rionald <vrionald@gmail.com>
2023-09-27 14:52:50 +0700

    Ganti beberapa field di AmiFakultas menjadi string

--- schemas/ami_fakultas.go
+++ schemas/ami_fakultas.go
@@ -1,19 +1,17 @@
 package model
 
-import "database/sql"
-
 type AmiFakultas struct {
-	IDFakultas int            `gorm:"primaryKey;column:id_fakultas" json:"-"`
-	UserName   string         `gorm:"column:user_name" json:"user_name"`
-	AmiConvert AmiConvert     `gorm:"joinForeignKey:user_name;foreignKey:id_rtm;references:UserName" json:"ami_convert_list"`
-	Fakultas   string         `gorm:"column:fakultas" json:"fakultas"`
-	Dekan      string         `gorm:"column:dekan" json:"dekan"`
-	Nidn       string         `gorm:"column:nidn" json:"nidn"`
-	Niknip     sql.NullString `gorm:"column:niknip" json:"niknip"`
-	Telp       sql.NullString `gorm:"column:telp" json:"telp"`
-	Email      sql.NullString `gorm:"column:email" json:"email"`
-	Foto       sql.NullString `gorm:"column:foto" json:"foto"`
-	IDSiklus   sql.NullInt32  `gorm:"column:id_siklus" json:"id_siklus"`
+	IDFakultas int        `gorm:"primaryKey;column:id_fakultas" json:"-"`
+	UserName   string     `gorm:"column:user_name" json:"user_name"`
+	AmiConvert AmiConvert `gorm:"joinForeignKey:user_name;foreignKey:id_rtm;references:UserName" json:"ami_convert_list"`
+	Fakultas   string     `gorm:"column:fakultas" json:"fakultas"`
+	Dekan      string     `gorm:"column:dekan" json:"dekan"`
+	Nidn       string     `gorm:"column:nidn" json:"nidn"`
+	Niknip     string     `gorm:"column:niknip" json:"niknip"`
+	Telp       string     `gorm:"column:telp" json:"telp"`
+	Email      string     `gorm:"column:email" json:"email"`
+	Foto       string     `gorm:"column:foto" json:"foto"`
+	IDSiklus   int        `gorm:"column:id_siklus" json:"id_siklus"`
 }
 
 func (m *AmiFakultas) TableName() string {
