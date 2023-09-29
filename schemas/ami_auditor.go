79beff637c3a98fadbe9164be4ee7810add3fa05
Valen Rionald <vrionald@gmail.com>
2023-09-29 09:42:07 +0700

    Ganti tipe data di AmiAuditor jadi string

--- schemas/ami_auditor.go
+++ schemas/ami_auditor.go
@@ -1,20 +1,18 @@
 package model
 
-import "database/sql"
-
 type AmiAuditor struct {
-	IDAuditor  int            `gorm:"primaryKey;column:id_auditor" json:"-"`
-	UserName   string         `gorm:"column:user_name" json:"user_name"`
-	AmiConvert AmiConvert     `gorm:"joinForeignKey:user_name;foreignKey:id_rtm;references:UserName" json:"ami_convert_list"`
-	IDFakultas int            `gorm:"column:id_fakultas" json:"id_fakultas"`
-	IDProdi    int            `gorm:"column:id_prodi" json:"id_prodi"`
-	Auditor    string         `gorm:"column:auditor" json:"auditor"`
-	Nidn       string         `gorm:"column:nidn" json:"nidn"`
-	Niknip     sql.NullString `gorm:"column:niknip" json:"niknip"`
-	Telp       sql.NullString `gorm:"column:telp" json:"telp"`
-	Email      sql.NullString `gorm:"column:email" json:"email"`
-	Foto       string         `gorm:"column:foto" json:"foto"`
-	IDSiklus   int            `gorm:"column:id_siklus" json:"id_siklus"`
+	IDAuditor  int        `gorm:"primaryKey;column:id_auditor" json:"-"`
+	UserName   string     `gorm:"column:user_name" json:"user_name"`
+	AmiConvert AmiConvert `gorm:"joinForeignKey:user_name;foreignKey:id_rtm;references:UserName" json:"ami_convert_list"`
+	IDFakultas int        `gorm:"column:id_fakultas" json:"id_fakultas"`
+	IDProdi    int        `gorm:"column:id_prodi" json:"id_prodi"`
+	Auditor    string     `gorm:"column:auditor" json:"auditor"`
+	Nidn       string     `gorm:"column:nidn" json:"nidn"`
+	Niknip     string     `gorm:"column:niknip" json:"niknip"`
+	Telp       string     `gorm:"column:telp" json:"telp"`
+	Email      string     `gorm:"column:email" json:"email"`
+	Foto       string     `gorm:"column:foto" json:"foto"`
+	IDSiklus   int        `gorm:"column:id_siklus" json:"id_siklus"`
 }
 
 func (m *AmiAuditor) TableName() string {
