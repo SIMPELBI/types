4693766e6e88ccba18b1ff375027b57a4f4d3daf
Valen Rionald <vrionald@gmail.com>
2023-10-03 10:32:42 +0700

    Tambah struct di ami prodi untuk kebutuhan join

--- schemas/ami_prodi.go
+++ schemas/ami_prodi.go
@@ -16,6 +16,23 @@ type AmiProdi struct {
 	IDSiklus   int        `gorm:"column:id_siklus" json:"id_siklus"`
 }
 
+type AmiProdiJoin struct {
+	IDProdi    int        `gorm:"primaryKey;column:id_prodi" json:"-"`
+	UserName   string     `gorm:"column:user_name" json:"user_name"`
+	AmiConvert AmiConvert `gorm:"joinForeignKey:user_name;foreignKey:id_rtm;references:UserName" json:"ami_convert_list"`
+	IDFakultas int        `gorm:"column:id_fakultas" json:"id_fakultas"`
+	Fakultas   string     `gorm:"column:fakultas" json:"fakultas"`
+	Prodi      string     `gorm:"column:prodi" json:"prodi"`
+	Jenjang    string     `gorm:"column:jenjang" json:"jenjang"`
+	Kaprodi    string     `gorm:"column:kaprodi" json:"kaprodi"`
+	Nidn       string     `gorm:"column:nidn" json:"nidn"`
+	Niknip     string     `gorm:"column:niknip" json:"niknip"`
+	Telp       string     `gorm:"column:telp" json:"telp"`
+	Email      string     `gorm:"column:email" json:"email"`
+	Foto       string     `gorm:"column:foto" json:"foto"`
+	IDSiklus   int        `gorm:"column:id_siklus" json:"id_siklus"`
+}
+
 func (m *AmiProdi) TableName() string {
 	return "ami_prodi"
 }
