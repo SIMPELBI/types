18ca1a69d632131d89840e9e3a30e6a7a40c555d
Valen Rionald <vrionald@gmail.com>
2023-10-04 14:42:52 +0700

    Memisahkan struct AmiAmi untuk join

--- schemas/ami_ami.go
+++ schemas/ami_ami.go
@@ -9,6 +9,19 @@ import (
 type AmiAmi struct {
 	IDAmi          int            `gorm:"primaryKey;column:id_ami" json:"-"`
 	IDFakultas     int            `gorm:"column:id_fakultas" json:"id_fakultas"`
+	IDProdi        int            `gorm:"column:id_prodi" json:"id_prodi"`
+	IDAuditorKetua int            `gorm:"column:id_auditor_ketua" json:"id_auditor_ketua"`
+	IDAnggota1     sql.NullInt32  `gorm:"column:id_anggota1" json:"id_anggota1"`
+	IDAnggota2     sql.NullInt32  `gorm:"column:id_anggota2" json:"id_anggota2"`
+	IDSiklus       int            `gorm:"column:id_siklus" json:"id_siklus"`
+	Status         sql.NullString `gorm:"column:status" json:"status"`
+	TglRtm         carbon.Carbon  `gorm:"column:tgl_rtm" json:"tgl_rtm"`
+	TglSelesai     sql.NullTime   `gorm:"column:tgl_selesai" json:"tgl_selesai"`
+}
+
+type AmiAmiJoin struct {
+	IDAmi          int            `gorm:"primaryKey;column:id_ami" json:"-"`
+	IDFakultas     int            `gorm:"column:id_fakultas" json:"id_fakultas"`
 	Fakultas       string         `gorm:"column:fakultas" json:"fakultas"`
 	IDProdi        int            `gorm:"column:id_prodi" json:"id_prodi"`
 	Prodi          string         `gorm:"column:prodi" json:"prodi"`
