9389d45c7e3907ca61068f76ea51a513b32564bc
Valen Rionald <vrionald@gmail.com>
2023-10-10 11:22:48 +0700

    Tambah struct untuk profile

--- schemas/ami_convert.go
+++ schemas/ami_convert.go
@@ -5,6 +5,12 @@ type AmiConvert struct {
 	UserLevel int    `gorm:"column:user_level" json:"user_level"`
 }
 
+type AmiConvertProfile struct {
+	IDRtm     string `gorm:"primaryKey;column:id_rtm" json:"-"`
+	UserLevel int    `gorm:"column:user_level" json:"user_level"`
+	NamaLevel string `gorm:"column:nama_level" json:"nama_level"`
+}
+
 func (m *AmiConvert) TableName() string {
 	return "ami_convert"
 }
