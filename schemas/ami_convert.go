2527a8686466377cd93857dd460c0154a7429e70
Valen Rionald <vrionald@gmail.com>
2023-10-10 11:48:24 +0700

    Coba tambah field untuk kebutuhan profile

--- schemas/ami_convert.go
+++ schemas/ami_convert.go
@@ -9,6 +9,10 @@ type AmiConvertProfile struct {
 	IDRtm     string `gorm:"primaryKey;column:id_rtm" json:"-"`
 	UserLevel int    `gorm:"column:user_level" json:"user_level"`
 	NamaLevel string `gorm:"column:nama_level" json:"nama_level"`
+	NamaUser  string `gorm:"column:nama_user" json:"nama_user"`
+	Email     string `gorm:"column:email" json:"email"`
+	Nidn      string `gorm:"column:nidn" json:"nidn"`
+	Foto      string `gorm:"column:foto" json:"foto"`
 }
 
 func (m *AmiConvert) TableName() string {
