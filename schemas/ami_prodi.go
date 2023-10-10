54433b639f295800f0b4a8e658eb4acef8301942
Valen Rionald <vrionald@gmail.com>
2023-10-10 10:48:45 +0700

    Membedakan struct di AmiProdi untuk Get dan Insert

--- schemas/ami_prodi.go
+++ schemas/ami_prodi.go
@@ -17,6 +17,22 @@ type AmiProdi struct {
 	IDSiklus   int        `gorm:"column:id_siklus" json:"id_siklus"`
 }
 
+type AmiProdiData struct {
+	IDProdi    int        `gorm:"primaryKey;column:id_prodi" json:"-"`
+	UserName   string     `gorm:"column:user_name" json:"user_name"`
+	AmiConvert AmiConvert `gorm:"joinForeignKey:user_name;foreignKey:id_rtm;references:UserName" json:"ami_convert_list"`
+	IDFakultas int        `gorm:"column:id_fakultas" json:"id_fakultas"`
+	Prodi      string     `gorm:"column:prodi" json:"prodi"`
+	IDJenjang  int        `gorm:"column:id_jenjang" json:"id_jenjang"`
+	Kaprodi    string     `gorm:"column:kaprodi" json:"kaprodi"`
+	Nidn       string     `gorm:"column:nidn" json:"nidn"`
+	Niknip     string     `gorm:"column:niknip" json:"niknip"`
+	Telp       string     `gorm:"column:telp" json:"telp"`
+	Email      string     `gorm:"column:email" json:"email"`
+	Foto       string     `gorm:"column:foto" json:"foto"`
+	IDSiklus   int        `gorm:"column:id_siklus" json:"id_siklus"`
+}
+
 func (m *AmiProdi) TableName() string {
 	return "ami_prodi"
 }
