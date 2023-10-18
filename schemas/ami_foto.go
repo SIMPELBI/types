1dff57a0298150e1a4fbf22b92acc4d422025fb6
Valen Rionald <vrionald@gmail.com>
2023-10-18 09:33:11 +0700

    Tambah struct untuk endpoint foto kegiatan

--- schemas/ami_foto.go
+++ schemas/ami_foto.go
@@ -10,6 +10,19 @@ type AmiFoto struct {
 	IDAuditor int           `gorm:"column:id_auditor" json:"id_auditor"`
 }
 
+type AmiFotoJoin struct {
+	IDFoto     int           `gorm:"primaryKey;column:id_foto" json:"-"`
+	IDAmi      int           `gorm:"column:id_ami" json:"id_ami"`
+	IDFakultas int           `gorm:"column:id_fakultas" json:"id_fakultas"`
+	Fakultas   string        `gorm:"column:fakultas" json:"fakultas"`
+	IDProdi    int           `gorm:"column:id_prodi" json:"id_prodi"`
+	Prodi      string        `gorm:"column:prodi" json:"prodi"`
+	Foto       string        `gorm:"column:foto" json:"foto"`
+	Tgl        carbon.Carbon `gorm:"column:tgl" json:"tgl"`
+	IDAuditor  int           `gorm:"column:id_auditor" json:"id_auditor"`
+	Auditor    string        `gorm:"column:auditor" json:"auditor"`
+}
+
 func (m *AmiFoto) TableName() string {
 	return "ami_foto"
 }
