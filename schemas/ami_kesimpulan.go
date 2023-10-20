20a780b93283eed619e71012c8c3d4af99869ddb
Valen Rionald <vrionald@gmail.com>
2023-10-20 10:39:39 +0700

    Fix(AmiKesimpulan): Perbaikan struct AmiKesimpulan

--- schemas/ami_kesimpulan.go
+++ schemas/ami_kesimpulan.go
@@ -1,17 +1,16 @@
 package model
 
 import (
-	"database/sql"
 	"github.com/golang-module/carbon/v2"
 )
 
 type AmiKesimpulan struct {
-	IDKesimpulan int            `gorm:"primaryKey;column:id_kesimpulan" json:"-"`
-	IDAmi        int            `gorm:"column:id_ami" json:"id_ami"`
-	CkpLengkap   string         `gorm:"column:ckp_lengkap" json:"ckp_lengkap"`
-	Sebutkan     sql.NullString `gorm:"column:sebutkan" json:"sebutkan"`
-	Tgl          carbon.Carbon  `gorm:"column:tgl" json:"tgl"`
-	IDAuditor    int            `gorm:"column:id_auditor" json:"id_auditor"`
+	IDKesimpulan int           `gorm:"primaryKey;column:id_kesimpulan" json:"-"`
+	IDAmi        int           `gorm:"column:id_ami" json:"id_ami"`
+	CkpLengkap   string        `gorm:"column:ckp_lengkap" json:"ckp_lengkap"`
+	Sebutkan     string        `gorm:"column:sebutkan" json:"sebutkan"`
+	Tgl          carbon.Carbon `gorm:"column:tgl" json:"tgl"`
+	IDAuditor    int           `gorm:"column:id_auditor" json:"id_auditor"`
 }
 
 func (m *AmiKesimpulan) TableName() string {
