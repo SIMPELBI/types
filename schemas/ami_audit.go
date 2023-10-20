41cb12ac3f2c0da8e715b9c6d5aab159d0388359
Valen Rionald <vrionald@gmail.com>
2023-10-20 10:20:30 +0700

    Fix(AmiAudit): Memperbaiki tipe field AmiAudit

--- schemas/ami_audit.go
+++ schemas/ami_audit.go
@@ -1,22 +1,21 @@
 package model
 
 import (
-	"database/sql"
 	"github.com/golang-module/carbon/v2"
 )
 
 type AmiAudit struct {
-	IDAudit      int            `gorm:"primaryKey;column:id_audit" json:"-"`
-	IDAmi        int            `gorm:"column:id_ami" json:"id_ami"`
-	IDStandar    int            `gorm:"column:id_standar" json:"id_standar"`
-	IDKts        int            `gorm:"column:id_kts" json:"id_kts"`
-	Uraian       string         `gorm:"column:uraian" json:"uraian"`
-	Tindakan     sql.NullString `gorm:"column:tindakan" json:"tindakan"`
-	Target       sql.NullString `gorm:"column:target" json:"target"`
-	Status       string         `gorm:"column:status" json:"status"`
-	Tgl          carbon.Carbon  `gorm:"column:tgl" json:"tgl"`
-	IDAuditor    int            `gorm:"column:id_auditor" json:"id_auditor"`
-	TglPerbaikan sql.NullTime   `gorm:"column:tgl_perbaikan" json:"tgl_perbaikan"`
+	IDAudit      int           `gorm:"primaryKey;column:id_audit" json:"-"`
+	IDAmi        int           `gorm:"column:id_ami" json:"id_ami"`
+	IDStandar    int           `gorm:"column:id_standar" json:"id_standar"`
+	IDKts        int           `gorm:"column:id_kts" json:"id_kts"`
+	Uraian       string        `gorm:"column:uraian" json:"uraian"`
+	Tindakan     string        `gorm:"column:tindakan" json:"tindakan"`
+	Target       string        `gorm:"column:target" json:"target"`
+	Status       string        `gorm:"column:status" json:"status"`
+	Tgl          carbon.Carbon `gorm:"column:tgl" json:"tgl"`
+	IDAuditor    int           `gorm:"column:id_auditor" json:"id_auditor"`
+	TglPerbaikan carbon.Carbon `gorm:"column:tgl_perbaikan" json:"tgl_perbaikan"`
 }
 
 func (m *AmiAudit) TableName() string {
