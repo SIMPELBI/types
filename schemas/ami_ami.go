a1e58b5e0a8567c4a57393e87d3d3c5bb404c962
Valen Rionald <vrionald@gmail.com>
2023-11-03 14:39:50 +0700

    FIX(AMIAMI): GANTI TIPE DATA ID AUDITOR ANGGOTA MENJADI INT

--- schemas/ami_ami.go
+++ schemas/ami_ami.go
@@ -27,9 +27,9 @@ type AmiAmiJoin struct {
 	Prodi          string        `gorm:"column:prodi" json:"prodi"`
 	IDAuditorKetua int           `gorm:"column:id_auditor_ketua" json:"id_auditor_ketua"`
 	NmAuditorKetua string        `gorm:"column:nm_auditor_ketua" json:"nm_auditor_ketua"`
-	IDAnggota1     sql.NullInt32 `gorm:"column:id_anggota1" json:"id_anggota1"`
+	IDAnggota1     int           `gorm:"column:id_anggota1" json:"id_anggota1"`
 	NmAuditor1     string        `gorm:"column:anggota1" json:"anggota1"`
-	IDAnggota2     sql.NullInt32 `gorm:"column:id_anggota2" json:"id_anggota2"`
+	IDAnggota2     int           `gorm:"column:id_anggota2" json:"id_anggota2"`
 	NmAuditor2     string        `gorm:"column:anggota2" json:"anggota2"`
 	IDSiklus       int           `gorm:"column:id_siklus" json:"id_siklus"`
 	Tahun          int           `gorm:"column:tahun" json:"tahun"`
