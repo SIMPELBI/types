85a41c7b7be314aba61caf08f539969f36e37074
Valen Rionald <vrionald@gmail.com>
2023-11-07 14:37:45 +0700

    Feat(AmiDokumenSpmi): Tambah field untuk Get Data

--- schemas/ami_dokumen_spmi.go
+++ schemas/ami_dokumen_spmi.go
@@ -8,7 +8,9 @@ type AmiDokumenSpmi struct {
 	Dokumen   string        `gorm:"column:dokumen" json:"dokumen"`
 	Tanggal   carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
 	IDAdmin   int           `gorm:"column:id_admin" json:"id_admin"`
+	NmAdmin   string        `gorm:"column:nm_admin" json:"nm_admin,omitempty"`
 	IDPeriode int           `gorm:"column:id_periode" json:"id_periode"`
+	Tahun     string        `gorm:"column:tahun" json:"tahun,omitempty"`
 }
 
 func (m *AmiDokumenSpmi) TableName() string {
