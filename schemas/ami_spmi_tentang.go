ac014177c5843d465de84338b17460aa5611206a
Valen Rionald <vrionald@gmail.com>
2023-11-09 10:57:57 +0700

    Fix(AmiSpmiTentang): Menghapus omitempty pada field tahun dan nm_admin

--- schemas/ami_spmi_tentang.go
+++ schemas/ami_spmi_tentang.go
@@ -19,9 +19,9 @@ type AmiSpmiTentangJoin struct {
 	Gambar    string        `gorm:"column:gambar" json:"gambar"`
 	Tanggal   carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
 	IDAdmin   int           `gorm:"column:id_admin" json:"id_admin"`
-	NmAdmin   string        `gorm:"column:nm_admin" json:"nm_admin,omitempty"`
+	NmAdmin   string        `gorm:"column:nm_admin" json:"nm_admin"`
 	IDPeriode int           `gorm:"column:id_periode" json:"id_periode"`
-	Tahun     string        `gorm:"column:tahun" json:"tahun,omitempty"`
+	Tahun     string        `gorm:"column:tahun" json:"tahun"`
 }
 
 func (m *AmiSpmiTentang) TableName() string {
