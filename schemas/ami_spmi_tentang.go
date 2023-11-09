3bfdfab4b80b62f4b1a9a3dd7069f1b17866b6a4
Valen Rionald <vrionald@gmail.com>
2023-11-09 10:57:15 +0700

    Fix(AmiSpmiTentang): Memperbaiki struct AmiSpmiTentang dan tambah struct untuk join

--- schemas/ami_spmi_tentang.go
+++ schemas/ami_spmi_tentang.go
@@ -9,9 +9,19 @@ type AmiSpmiTentang struct {
 	Gambar    string        `gorm:"column:gambar" json:"gambar"`
 	Tanggal   carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
 	IDAdmin   int           `gorm:"column:id_admin" json:"id_admin"`
-	NmAdmin   string        `gorm:"-" json:"nm_admin"`
 	IDPeriode int           `gorm:"column:id_periode" json:"id_periode"`
-	Tahun     string        `gorm:"-" json:"tahun"`
+}
+
+type AmiSpmiTentangJoin struct {
+	IDTentang int           `gorm:"primaryKey;column:id_tentang" json:"-"`
+	Judul     string        `gorm:"column:judul" json:"judul"`
+	Isi       string        `gorm:"column:isi" json:"isi"`
+	Gambar    string        `gorm:"column:gambar" json:"gambar"`
+	Tanggal   carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
+	IDAdmin   int           `gorm:"column:id_admin" json:"id_admin"`
+	NmAdmin   string        `gorm:"column:nm_admin" json:"nm_admin,omitempty"`
+	IDPeriode int           `gorm:"column:id_periode" json:"id_periode"`
+	Tahun     string        `gorm:"column:tahun" json:"tahun,omitempty"`
 }
 
 func (m *AmiSpmiTentang) TableName() string {
