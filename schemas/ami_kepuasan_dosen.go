eb5e1aa76b40d4a02772ba6333e1a57b97f76e0b
Valen Rionald <vrionald@gmail.com>
2023-11-13 11:14:43 +0700

    Feat(AmiKepuasanDosenJoin): Tambah struct untuk Join tabel

--- schemas/ami_kepuasan_dosen.go
+++ schemas/ami_kepuasan_dosen.go
@@ -10,3 +10,14 @@ type AmiKepuasanDosen struct {
 	IDAdmin         int           `gorm:"column:id_admin" json:"id_admin"`
 	IDPeriode       int           `gorm:"column:id_periode" json:"id_periode"`
 }
+
+type AmiKepuasanDosenJoin struct {
+	IDKepuasanDosen int           `gorm:"primaryKey;column:id_kepuasan_dosen" json:"-"`
+	Judul           string        `gorm:"column:judul" json:"judul"`
+	File            string        `gorm:"column:file" json:"file"`
+	Tanggal         carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
+	IDAdmin         int           `gorm:"column:id_admin" json:"id_admin"`
+	NamaAdmin       string        `gorm:"column:nm_admin" json:"nm_admin"`
+	IDPeriode       int           `gorm:"column:id_periode" json:"id_periode"`
+	Tahun           string        `gorm:"column:tahun" json:"tahun"`
+}
