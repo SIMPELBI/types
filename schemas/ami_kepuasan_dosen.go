ecc426f4ba49a0451a2de76e2d0ac78083de621f
Valen Rionald <vrionald@gmail.com>
2023-11-13 10:43:47 +0700

    Feat(AmiKepuasanDosen): Tambah struct untuk kepuasan dosen

---
+++ schemas/ami_kepuasan_dosen.go
@@ -0,0 +1,14 @@
+package model
+
+import "github.com/golang-module/carbon/v2"
+
+type AmiKepuasanDosen struct {
+	IDDokumen int           `gorm:"primaryKey;column:id_dokumen" json:"-"`
+	Judul     string        `gorm:"column:judul" json:"judul"`
+	Dokumen   string        `gorm:"column:dokumen" json:"dokumen"`
+	Tanggal   carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
+	IDAdmin   int           `gorm:"column:id_admin" json:"id_admin"`
+	NmAdmin   string        `gorm:"column:nm_admin" json:"nm_admin,omitempty"`
+	IDPeriode int           `gorm:"column:id_periode" json:"id_periode"`
+	Tahun     string        `gorm:"column:tahun" json:"tahun,omitempty"`
+}
