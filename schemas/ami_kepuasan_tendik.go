c4d94ac42d99383f72fad8cd2f084b31b1fd0c72
Valen Rionald <vrionald@gmail.com>
2023-11-15 09:21:20 +0700

    Feat(AmiKepuasanTendik): Tambah struct untuk get all data

--- schemas/ami_kepuasan_tendik.go
+++ schemas/ami_kepuasan_tendik.go
@@ -10,3 +10,14 @@ type AmiKepuasanTendik struct {
 	IDAdmin          int           `gorm:"column:id_admin" json:"id_admin"`
 	IDPeriode        int           `gorm:"column:id_periode" json:"id_periode"`
 }
+
+type AmiKepuasanTendikJoin struct {
+	IDKepuasanTendik int           `gorm:"primaryKey;column:id_kepuasan_tendik" json:"-"`
+	Judul            string        `gorm:"column:judul" json:"judul"`
+	File             string        `gorm:"column:file" json:"file"`
+	Tanggal          carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
+	IDAdmin          int           `gorm:"column:id_admin" json:"id_admin"`
+	NamaAdmin        string        `gorm:"column:nm_admin" json:"nm_admin"`
+	IDPeriode        int           `gorm:"column:id_periode" json:"id_periode"`
+	Tahun            string        `gorm:"column:tahun" json:"tahun"`
+}
