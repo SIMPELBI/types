1e71214c37bf1d49cbcab7fd3a148b180cb3a9fc
Valen Rionald <vrionald@gmail.com>
2023-11-17 11:01:16 +0700

    Feat(AmiDokumenSpmi): Tambah field untuk link dokumen

--- schemas/ami_dokumen_spmi.go
+++ schemas/ami_dokumen_spmi.go
@@ -3,14 +3,15 @@ package model
 import "github.com/golang-module/carbon/v2"
 
 type AmiDokumenSpmi struct {
-	IDDokumen int           `gorm:"primaryKey;column:id_dokumen" json:"-"`
-	Judul     string        `gorm:"column:judul" json:"judul"`
-	Dokumen   string        `gorm:"column:dokumen" json:"dokumen"`
-	Tanggal   carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
-	IDAdmin   int           `gorm:"column:id_admin" json:"id_admin"`
-	NmAdmin   string        `gorm:"column:nm_admin" json:"nm_admin,omitempty"`
-	IDPeriode int           `gorm:"column:id_periode" json:"id_periode"`
-	Tahun     string        `gorm:"column:tahun" json:"tahun,omitempty"`
+	IDDokumen   int           `gorm:"primaryKey;column:id_dokumen" json:"-"`
+	Judul       string        `gorm:"column:judul" json:"judul"`
+	Dokumen     string        `gorm:"column:dokumen" json:"dokumen"`
+	Tanggal     carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
+	IDAdmin     int           `gorm:"column:id_admin" json:"id_admin"`
+	NmAdmin     string        `gorm:"column:nm_admin" json:"nm_admin,omitempty"`
+	IDPeriode   int           `gorm:"column:id_periode" json:"id_periode"`
+	Tahun       string        `gorm:"column:tahun" json:"tahun,omitempty"`
+	LinkDokumen string        `gorm:"column:link_dokumen" json:"link_dokumen"`
 }
 
 func (m *AmiDokumenSpmi) TableName() string {
