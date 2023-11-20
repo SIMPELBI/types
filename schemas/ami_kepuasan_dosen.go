4e571d5eaa2d181a1b9691f1be1fb30b5eb696cf
Valen Rionald <vrionald@gmail.com>
2023-11-20 14:22:10 +0700

    Fix(AmiKepuasanDosen): Memperbaiki struct ami kepuasan dosen

--- schemas/ami_kepuasan_dosen.go
+++ schemas/ami_kepuasan_dosen.go
@@ -8,9 +8,18 @@ type AmiKepuasanDosen struct {
 	File            string        `gorm:"column:file" json:"file"`
 	Tanggal         carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
 	IDAdmin         int           `gorm:"column:id_admin" json:"id_admin"`
-	NamaAdmin       string        `gorm:"column:nm_admin" json:"nm_admin,omitempty"`
 	IDPeriode       int           `gorm:"column:id_periode" json:"id_periode"`
-	Tahun           string        `gorm:"column:tahun" json:"tahun,omitempty"`
+}
+
+type AmiKepuasanDosenJoin struct {
+	IDKepuasanDosen int           `gorm:"primaryKey;column:id_kepuasan_dosen" json:"-"`
+	Judul           string        `gorm:"column:judul" json:"judul"`
+	File            string        `gorm:"column:file" json:"file"`
+	Tanggal         carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
+	IDAdmin         int           `gorm:"column:id_admin" json:"id_admin"`
+	NamaAdmin       string        `gorm:"column:nm_admin" json:"nm_admin"`
+	IDPeriode       int           `gorm:"column:id_periode" json:"id_periode"`
+	Tahun           string        `gorm:"column:tahun" json:"tahun"`
 }
 
 func (m *AmiKepuasanDosen) TableName() string {
