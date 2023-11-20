1c653b15478343990ccb9a26a789fddc5d61cda4
Valen Rionald <vrionald@gmail.com>
2023-11-20 14:22:54 +0700

    Fix(AmiDokumenSpmi): Memperbaiki struct ami dokumen spmi

--- schemas/ami_dokumen_spmi.go
+++ schemas/ami_dokumen_spmi.go
@@ -8,9 +8,19 @@ type AmiDokumenSpmi struct {
 	Dokumen     string        `gorm:"column:dokumen" json:"dokumen"`
 	Tanggal     carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
 	IDAdmin     int           `gorm:"column:id_admin" json:"id_admin"`
-	NmAdmin     string        `gorm:"column:nm_admin" json:"nm_admin,omitempty"`
 	IDPeriode   int           `gorm:"column:id_periode" json:"id_periode"`
-	Tahun       string        `gorm:"column:tahun" json:"tahun,omitempty"`
+	LinkDokumen string        `gorm:"column:link_dokumen" json:"link_dokumen"`
+}
+
+type AmiDokumenSpmiJoin struct {
+	IDDokumen   int           `gorm:"primaryKey;column:id_dokumen" json:"-"`
+	Judul       string        `gorm:"column:judul" json:"judul"`
+	Dokumen     string        `gorm:"column:dokumen" json:"dokumen"`
+	Tanggal     carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
+	IDAdmin     int           `gorm:"column:id_admin" json:"id_admin"`
+	NmAdmin     string        `gorm:"column:nm_admin" json:"nm_admin"`
+	IDPeriode   int           `gorm:"column:id_periode" json:"id_periode"`
+	Tahun       string        `gorm:"column:tahun" json:"tahun"`
 	LinkDokumen string        `gorm:"column:link_dokumen" json:"link_dokumen"`
 }
 
