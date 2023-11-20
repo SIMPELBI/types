d77bd817d65cfbe6c42e60bd9325ba15108f9c80
Valen Rionald <vrionald@gmail.com>
2023-11-20 13:57:23 +0700

    Feat(AmiKepuasanMahasiswa): Tambah struct untuk get all data kepuasan mahasiswa

--- schemas/ami_kepuasan_mahasiswa.go
+++ schemas/ami_kepuasan_mahasiswa.go
@@ -10,3 +10,18 @@ type AmiKepuasanMahasiswa struct {
 	IDAdmin             int           `gorm:"column:id_admin" json:"id_admin"`
 	IDPeriode           int           `gorm:"column:id_periode" json:"id_periode"`
 }
+
+type AmiKepuasanMahasiswaJoin struct {
+	IDKepuasanMahasiswa int           `gorm:"primaryKey;column:id_kepuasan_mahasiswa" json:"-"`
+	Judul               string        `gorm:"column:judul" json:"judul"`
+	File                string        `gorm:"column:file" json:"file"`
+	Tanggal             carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
+	IDAdmin             int           `gorm:"column:id_admin" json:"id_admin"`
+	NamaAdmin           string        `gorm:"column:nm_admin" json:"nm_admin"`
+	IDPeriode           int           `gorm:"column:id_periode" json:"id_periode"`
+	Tahun               string        `gorm:"column:tahun" json:"tahun"`
+}
+
+func (m *AmiKepuasanMahasiswa) TableName() string {
+	return "ami_kepuasan_mahasiswa"
+}
