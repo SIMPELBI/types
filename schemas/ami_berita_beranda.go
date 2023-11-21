41f7357e961703b92201cf299ec90ed17488fb60
Valen Rionald <vrionald@gmail.com>
2023-11-21 13:08:10 +0700

    Feat(AmiBeritaSpmi): Tambah struct untuk join tabel

--- schemas/ami_berita_beranda.go
+++ schemas/ami_berita_beranda.go
@@ -12,6 +12,18 @@ type AmiBeritaSpmi struct {
 	IDPeriode       int           `gorm:"column:id_periode" json:"id_periode"`
 }
 
+type AmiBeritaSpmiJoin struct {
+	IDBeritaBeranda int           `gorm:"primaryKey;column:id_berita_beranda" json:"-"`
+	Judul           string        `gorm:"column:judul" json:"judul"`
+	Isi             string        `gorm:"column:isi" json:"isi"`
+	Gambar          string        `gorm:"column:gambar" json:"gambar"`
+	Tanggal         carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
+	IDAdmin         int           `gorm:"column:id_admin" json:"id_admin"`
+	NamaAdmin       string        `gorm:"column:nm_admin" json:"nm_admin"`
+	IDPeriode       int           `gorm:"column:id_periode" json:"id_periode"`
+	Tahun           string        `gorm:"column:tahun" json:"tahun"`
+}
+
 func (m *AmiBeritaSpmi) TableName() string {
 	return "ami_berita_spmi"
 }
