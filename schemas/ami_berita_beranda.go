f336f24ef9d3d2593c61fddec7d7890bc763a36c
Valen Rionald <vrionald@gmail.com>
2023-11-21 13:13:09 +0700

    Fix(BeritaSpmi): Ganti nama field id menjadi id_berita_spmi

--- schemas/ami_berita_beranda.go
+++ schemas/ami_berita_beranda.go
@@ -3,25 +3,25 @@ package model
 import "github.com/golang-module/carbon/v2"
 
 type AmiBeritaSpmi struct {
-	IDBeritaBeranda int           `gorm:"primaryKey;column:id_berita_beranda" json:"-"`
-	Judul           string        `gorm:"column:judul" json:"judul"`
-	Isi             string        `gorm:"column:isi" json:"isi"`
-	Gambar          string        `gorm:"column:gambar" json:"gambar"`
-	Tanggal         carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
-	IDAdmin         int           `gorm:"column:id_admin" json:"id_admin"`
-	IDPeriode       int           `gorm:"column:id_periode" json:"id_periode"`
+	IDBeritaSpmi int           `gorm:"primaryKey;column:id_berita_spmi" json:"-"`
+	Judul        string        `gorm:"column:judul" json:"judul"`
+	Isi          string        `gorm:"column:isi" json:"isi"`
+	Gambar       string        `gorm:"column:gambar" json:"gambar"`
+	Tanggal      carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
+	IDAdmin      int           `gorm:"column:id_admin" json:"id_admin"`
+	IDPeriode    int           `gorm:"column:id_periode" json:"id_periode"`
 }
 
 type AmiBeritaSpmiJoin struct {
-	IDBeritaBeranda int           `gorm:"primaryKey;column:id_berita_beranda" json:"-"`
-	Judul           string        `gorm:"column:judul" json:"judul"`
-	Isi             string        `gorm:"column:isi" json:"isi"`
-	Gambar          string        `gorm:"column:gambar" json:"gambar"`
-	Tanggal         carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
-	IDAdmin         int           `gorm:"column:id_admin" json:"id_admin"`
-	NamaAdmin       string        `gorm:"column:nm_admin" json:"nm_admin"`
-	IDPeriode       int           `gorm:"column:id_periode" json:"id_periode"`
-	Tahun           string        `gorm:"column:tahun" json:"tahun"`
+	IDBeritaSpmi int           `gorm:"primaryKey;column:id_berita_spmi" json:"-"`
+	Judul        string        `gorm:"column:judul" json:"judul"`
+	Isi          string        `gorm:"column:isi" json:"isi"`
+	Gambar       string        `gorm:"column:gambar" json:"gambar"`
+	Tanggal      carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
+	IDAdmin      int           `gorm:"column:id_admin" json:"id_admin"`
+	NamaAdmin    string        `gorm:"column:nm_admin" json:"nm_admin"`
+	IDPeriode    int           `gorm:"column:id_periode" json:"id_periode"`
+	Tahun        string        `gorm:"column:tahun" json:"tahun"`
 }
 
 func (m *AmiBeritaSpmi) TableName() string {
