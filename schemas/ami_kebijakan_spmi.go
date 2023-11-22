3de457dad5456e023f926e2f4a04d3093bf66ded
Valen Rionald <vrionald@gmail.com>
2023-11-22 10:30:38 +0700

    Feat(DokumenKebijakanSpmi): Tambah struct untuk join data kebijakan spmi

--- schemas/ami_kebijakan_spmi.go
+++ schemas/ami_kebijakan_spmi.go
@@ -12,3 +12,16 @@ type AmiKebijakanSpmi struct {
 	IDAdmin         int           `gorm:"column:id_admin" json:"id_admin"`
 	IDPeriode       int           `gorm:"column:id_periode" json:"id_periode"`
 }
+
+type AmiKebijakanSpmiJoin struct {
+	IDKebijakanSpmi int           `gorm:"primaryKey;column:id_kebijakan_spmi" json:"-"`
+	Subject         string        `gorm:"column:subject" json:"subject"`
+	Keterangan      string        `gorm:"column:keterangan" json:"keterangan"`
+	File            string        `gorm:"column:file" json:"file"`
+	LinkDokumen     string        `gorm:"column:link_dokumen" json:"link_dokumen"`
+	Tanggal         carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
+	IDAdmin         int           `gorm:"column:id_admin" json:"id_admin"`
+	NamaAdmin       string        `gorm:"column:nm_admin" json:"nm_admin"`
+	IDPeriode       int           `gorm:"column:id_periode" json:"id_periode"`
+	Tahun           string        `gorm:"column:tahun" json:"tahun"`
+}
