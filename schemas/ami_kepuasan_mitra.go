2321cdbbf91a1033bca607792e6331d520ed54c0
Valen Rionald <vrionald@gmail.com>
2023-11-22 14:26:21 +0700

    Feat(KepuasanMitra): Tambah struct untuk join

--- schemas/ami_kepuasan_mitra.go
+++ schemas/ami_kepuasan_mitra.go
@@ -6,9 +6,22 @@ type AmiKepuasanMitra struct {
 	IDKepuasanMitra int           `gorm:"primaryKey;column:id_kepuasan_mitra" json:"-"`
 	Judul           string        `gorm:"column:judul" json:"judul"`
 	File            string        `gorm:"column:file" json:"file"`
+	Keterangan      string        `gorm:"column:keterangan" json:"keterangan"`
 	Tanggal         carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
 	IDAdmin         int           `gorm:"column:id_admin" json:"id_admin"`
 	IDPeriode       int           `gorm:"column:id_periode" json:"id_periode"`
+}
+
+type AmiKepuasanMitraJoin struct {
+	IDKepuasanMitra int           `gorm:"primaryKey;column:id_kepuasan_mitra" json:"-"`
+	Judul           string        `gorm:"column:judul" json:"judul"`
+	File            string        `gorm:"column:file" json:"file"`
+	Keterangan      string        `gorm:"column:keterangan" json:"keterangan"`
+	Tanggal         carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
+	IDAdmin         int           `gorm:"column:id_admin" json:"id_admin"`
+	NamaAdmin       string        `gorm:"column:nm_admin" json:"nm_admin"`
+	IDPeriode       int           `gorm:"column:id_periode" json:"id_periode"`
+	Tahun           string        `gorm:"column:tahun" json:"tahun"`
 }
 
 func (m *AmiKepuasanMitra) TableName() string {
