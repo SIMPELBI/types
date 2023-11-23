5a97757119d79c7682c506d558b629bed1b5aa35
Valen Rionald <vrionald@gmail.com>
2023-11-23 15:33:19 +0700

    Fix(DokumenSpmi): Hapus field link dokumen

--- schemas/ami_dokumen_spmi.go
+++ schemas/ami_dokumen_spmi.go
@@ -3,27 +3,25 @@ package model
 import "github.com/golang-module/carbon/v2"
 
 type AmiDokumenSpmi struct {
-	IDDokumen   int           `gorm:"primaryKey;column:id_dokumen" json:"-"`
-	Judul       string        `gorm:"column:judul" json:"judul"`
-	File        string        `gorm:"column:file" json:"file"`
-	Tanggal     carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
-	IDAdmin     int           `gorm:"column:id_admin" json:"id_admin"`
-	IDPeriode   int           `gorm:"column:id_periode" json:"id_periode"`
-	LinkDokumen string        `gorm:"column:link_dokumen" json:"link_dokumen"`
-	Keterangan  string        `gorm:"column:keterangan" json:"keterangan"`
+	IDDokumen  int           `gorm:"primaryKey;column:id_dokumen" json:"-"`
+	Judul      string        `gorm:"column:judul" json:"judul"`
+	File       string        `gorm:"column:file" json:"file"`
+	Tanggal    carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
+	IDAdmin    int           `gorm:"column:id_admin" json:"id_admin"`
+	IDPeriode  int           `gorm:"column:id_periode" json:"id_periode"`
+	Keterangan string        `gorm:"column:keterangan" json:"keterangan"`
 }
 
 type AmiDokumenSpmiJoin struct {
-	IDDokumen   int           `gorm:"primaryKey;column:id_dokumen" json:"-"`
-	Judul       string        `gorm:"column:judul" json:"judul"`
-	File        string        `gorm:"column:file" json:"file"`
-	Tanggal     carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
-	IDAdmin     int           `gorm:"column:id_admin" json:"id_admin"`
-	NmAdmin     string        `gorm:"column:nm_admin" json:"nm_admin"`
-	IDPeriode   int           `gorm:"column:id_periode" json:"id_periode"`
-	Tahun       string        `gorm:"column:tahun" json:"tahun"`
-	LinkDokumen string        `gorm:"column:link_dokumen" json:"link_dokumen"`
-	Keterangan  string        `gorm:"column:keterangan" json:"keterangan"`
+	IDDokumen  int           `gorm:"primaryKey;column:id_dokumen" json:"-"`
+	Judul      string        `gorm:"column:judul" json:"judul"`
+	File       string        `gorm:"column:file" json:"file"`
+	Tanggal    carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
+	IDAdmin    int           `gorm:"column:id_admin" json:"id_admin"`
+	NmAdmin    string        `gorm:"column:nm_admin" json:"nm_admin"`
+	IDPeriode  int           `gorm:"column:id_periode" json:"id_periode"`
+	Tahun      string        `gorm:"column:tahun" json:"tahun"`
+	Keterangan string        `gorm:"column:keterangan" json:"keterangan"`
 }
 
 func (m *AmiDokumenSpmi) TableName() string {
