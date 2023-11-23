5b81a923234d59f8b4d7b18db6dc6a38ffadd7e2
Valen Rionald <vrionald@gmail.com>
2023-11-23 15:28:00 +0700

    Fix(DokumenSpmi): Ganti nama field dokumen jadi file

--- schemas/ami_dokumen_spmi.go
+++ schemas/ami_dokumen_spmi.go
@@ -5,7 +5,7 @@ import "github.com/golang-module/carbon/v2"
 type AmiDokumenSpmi struct {
 	IDDokumen   int           `gorm:"primaryKey;column:id_dokumen" json:"-"`
 	Judul       string        `gorm:"column:judul" json:"judul"`
-	Dokumen     string        `gorm:"column:dokumen" json:"dokumen"`
+	File        string        `gorm:"column:file" json:"file"`
 	Tanggal     carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
 	IDAdmin     int           `gorm:"column:id_admin" json:"id_admin"`
 	IDPeriode   int           `gorm:"column:id_periode" json:"id_periode"`
@@ -16,7 +16,7 @@ type AmiDokumenSpmi struct {
 type AmiDokumenSpmiJoin struct {
 	IDDokumen   int           `gorm:"primaryKey;column:id_dokumen" json:"-"`
 	Judul       string        `gorm:"column:judul" json:"judul"`
-	Dokumen     string        `gorm:"column:dokumen" json:"dokumen"`
+	File        string        `gorm:"column:file" json:"file"`
 	Tanggal     carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
 	IDAdmin     int           `gorm:"column:id_admin" json:"id_admin"`
 	NmAdmin     string        `gorm:"column:nm_admin" json:"nm_admin"`
