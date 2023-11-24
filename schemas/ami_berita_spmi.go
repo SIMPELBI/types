bde648a9e6d31418c96225172c44aecda01dd41c
Valen Rionald <vrionald@gmail.com>
2023-11-24 11:07:03 +0700

    Fix(BeritaSpmi): Menghapus field periode siklus

--- schemas/ami_berita_spmi.go
+++ schemas/ami_berita_spmi.go
@@ -9,7 +9,6 @@ type AmiBeritaSpmi struct {
 	Gambar       string        `gorm:"column:gambar" json:"gambar"`
 	Tanggal      carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
 	IDAdmin      int           `gorm:"column:id_admin" json:"id_admin"`
-	IDPeriode    int           `gorm:"column:id_periode" json:"id_periode"`
 }
 
 type AmiBeritaSpmiJoin struct {
@@ -20,8 +19,6 @@ type AmiBeritaSpmiJoin struct {
 	Tanggal      carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
 	IDAdmin      int           `gorm:"column:id_admin" json:"id_admin"`
 	NamaAdmin    string        `gorm:"column:nm_admin" json:"nm_admin"`
-	IDPeriode    int           `gorm:"column:id_periode" json:"id_periode"`
-	Tahun        string        `gorm:"column:tahun" json:"tahun"`
 }
 
 func (m *AmiBeritaSpmi) TableName() string {
