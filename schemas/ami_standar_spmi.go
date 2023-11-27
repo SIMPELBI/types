bd85fff22a2fde109e549987bf6bc6c2785679e7
Valen Rionald <vrionald@gmail.com>
2023-11-27 11:16:35 +0700

    Feat(StandarSpmi): Tambah struct untuk join data

--- schemas/ami_standar_spmi.go
+++ schemas/ami_standar_spmi.go
@@ -12,6 +12,18 @@ type AmiStandarSpmi struct {
 	IDPeriode     int           `gorm:"column:id_periode" json:"id_periode"`
 }
 
+type AmiStandarSpmiJoin struct {
+	IDStandarSpmi int           `gorm:"primaryKey;column:id_standar_spmi" json:"-"`
+	Judul         string        `gorm:"column:judul" json:"judul"`
+	Keterangan    string        `gorm:"column:keterangan" json:"keterangan"`
+	File          string        `gorm:"column:file" json:"file"`
+	Tanggal       carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
+	IDAdmin       int           `gorm:"column:id_admin" json:"id_admin"`
+	NamaAdmin     string        `gorm:"column:nm_admin" json:"nm_admin"`
+	IDPeriode     int           `gorm:"column:id_periode" json:"id_periode"`
+	Tahun         string        `gorm:"column:tahun" json:"tahun"`
+}
+
 func (m *AmiStandarSpmi) TableName() string {
 	return "ami_standar_spmi"
 }
