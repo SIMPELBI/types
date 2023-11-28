3e6e446d77f9457bacd57e9d008fdfd84010a8d5
Valen Rionald <vrionald@gmail.com>
2023-11-28 15:32:44 +0700

    Fix(StrukturDirektoratPsms): Ganti nama tim spmi jadi Struktur Direktorat PSMS

--- schemas/ami_struktur_direktorat_psms.go
+++ schemas/ami_struktur_direktorat_psms.go
@@ -1,12 +1,12 @@
 package model
 
-type AmiTimSpmi struct {
+type AmiStrukturDirektoratPsms struct {
 	IDAnggota int    `gorm:"primaryKey;column:id_anggota" json:"-"`
 	Nama      string `gorm:"column:nama" json:"nama"`
 	Jabatan   string `gorm:"column:jabatan" json:"jabatan"`
 	Foto      string `gorm:"column:foto" json:"foto"`
 }
 
-func (*AmiTimSpmi) TableName() string {
-	return "ami_tim_spmi"
+func (*AmiStrukturDirektoratPsms) TableName() string {
+	return "ami_struktur_direktorat_psms"
 }
