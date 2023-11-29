6d6e268d6b701d1cd79b878a30b555849578156f
Valen Rionald <vrionald@gmail.com>
2023-11-29 11:00:21 +0700

    Feat(LamTeknik): Tambah struct untuk join data

--- schemas/ami_lam_teknik.go
+++ schemas/ami_lam_teknik.go
@@ -14,6 +14,21 @@ type AmiLamTeknik struct {
 	IDAdmin          int           `gorm:"column:id_admin" json:"id_admin"`
 }
 
+type AmiLamTeknikJoin struct {
+	IDLamTeknik      int           `gorm:"primaryKey;column:id_lamteknik" json:"-"`
+	SkSertifikat     string        `gorm:"column:sk_sertifikat" json:"sk_sertifikat"`
+	Peringkat        string        `gorm:"column:peringkat" json:"peringkat"`
+	Konten           string        `gorm:"column:konten" json:"konten"`
+	MasaBerlakuStart carbon.Carbon `gorm:"column:masa_berlaku_start" json:"masa_berlaku_start"`
+	MasaBerlakuEnd   carbon.Carbon `gorm:"column:masa_berlaku_end" json:"masa_berlaku_end"`
+	IDJenjang        int           `gorm:"column:id_jenjang" json:"id_jenjang"`
+	Jenjang          string        `gorm:"column:jenjang" json:"jenjang"`
+	IDProdi          int           `gorm:"column:id_prodi" json:"id_prodi"`
+	Prodi            string        `gorm:"column:prodi" json:"prodi"`
+	IDAdmin          int           `gorm:"column:id_admin" json:"id_admin"`
+	NamaAdmin        string        `gorm:"column:nm_admin" json:"nm_admin"`
+}
+
 func (m *AmiLamTeknik) TableName() string {
 	return "ami_lam_teknik"
 }
