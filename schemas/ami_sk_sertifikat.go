9ec8796966fa55e6c35930dcfc4ed86cbc9c3b19
Valen Rionald <vrionald@gmail.com>
2023-11-29 11:17:53 +0700

    Feat(SkSertifikat): Tambah struct untuk join data

--- schemas/ami_sk_sertifikat.go
+++ schemas/ami_sk_sertifikat.go
@@ -9,3 +9,16 @@ type AmiSkSertifikat struct {
 	Tanggal          carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
 	IDAdmin          int           `gorm:"column:id_admin" json:"id_admin"`
 }
+
+type AmiSkSertifikatJoin struct {
+	IDSkSertifikat   int           `gorm:"primaryKey;column:id_sk_sertifikat" json:"-"`
+	NamaSkSertifikat string        `gorm:"column:nama_sk_sertifikat" json:"nama_sk_sertifikat"`
+	File             string        `gorm:"column:file" json:"file"`
+	Tanggal          carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
+	IDAdmin          int           `gorm:"column:id_admin" json:"id_admin"`
+	NamaAdmin        string        `gorm:"column:nm_admin" json:"nm_admin"`
+}
+
+func (m *AmiSkSertifikat) TableName() string {
+	return "ami_sk_sertifikat"
+}
