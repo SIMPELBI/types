713b5f4f1bd3481b46c8af43406b5674c3e4514b
Valen Rionald <vrionald@gmail.com>
2023-12-01 13:32:42 +0700

    Feat(LamInfokom): Tambah struct untuk join data

--- schemas/ami_lam_infokom.go
+++ schemas/ami_lam_infokom.go
@@ -14,6 +14,22 @@ type AmiLamInfokom struct {
 	IDAdmin          int           `gorm:"column:id_admin" json:"id_admin"`
 }
 
+type AmiLamInfokomJoin struct {
+	IDLamInfokom     int           `gorm:"primaryKey;column:id_laminfokom" json:"-"`
+	IDSkSertifikat   int           `gorm:"column:id_sk_sertifikat" json:"id_sk_sertifikat"`
+	NamaSkSertifikat string        `gorm:"column:nama_sk_sertifikat" json:"nama_sk_sertifikat"`
+	Peringkat        string        `gorm:"column:peringkat" json:"peringkat"`
+	Konten           string        `gorm:"column:konten" json:"konten"`
+	MasaBerlakuStart carbon.Carbon `gorm:"column:masa_berlaku_start" json:"masa_berlaku_start"`
+	MasaBerlakuEnd   carbon.Carbon `gorm:"column:masa_berlaku_end" json:"masa_berlaku_end"`
+	IDJenjang        int           `gorm:"column:id_jenjang" json:"id_jenjang"`
+	Jenjang          string        `gorm:"column:jenjang" json:"jenjang"`
+	IDProdi          int           `gorm:"column:id_prodi" json:"id_prodi"`
+	Prodi            string        `gorm:"column:prodi" json:"prodi"`
+	IDAdmin          int           `gorm:"column:id_admin" json:"id_admin"`
+	NamaAdmin        string        `gorm:"column:nm_admin" json:"nm_admin"`
+}
+
 func (m *AmiLamInfokom) TableName() string {
 	return "ami_lam_infokom"
 }
