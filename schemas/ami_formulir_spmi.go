a2bea4aa04f7e260a8a77c51cc8b5b2307257def
Valen Rionald <vrionald@gmail.com>
2023-12-08 09:17:41 +0700

    Feat(FormulirSPMI): Tambah struct untuk join data

--- schemas/ami_formulir_spmi.go
+++ schemas/ami_formulir_spmi.go
@@ -12,6 +12,18 @@ type AmiFormulirSpmi struct {
 	IDPeriode      int           `gorm:"column:id_periode" json:"id_periode"`
 }
 
+type AmiFormulirSpmiJoin struct {
+	IDFormulirSpmi int           `gorm:"primaryKey;column:id_formulir_spmi" json:"-"`
+	Judul          string        `gorm:"column:judul" json:"judul"`
+	Keterangan     string        `gorm:"column:keterangan" json:"keterangan"`
+	File           string        `gorm:"column:file" json:"file"`
+	Tanggal        carbon.Carbon `gorm:"column:tanggal" json:"tanggal"`
+	IDAdmin        int           `gorm:"column:id_admin" json:"id_admin"`
+	NamaAdmin      string        `gorm:"column:nm_admin" json:"nm_admin"`
+	IDPeriode      int           `gorm:"column:id_periode" json:"id_periode"`
+	Tahun          string        `gorm:"column:tahun" json:"tahun"`
+}
+
 func (m *AmiFormulirSpmi) TableName() string {
 	return "ami_formulir_spmi"
 }
