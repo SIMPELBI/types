e3efb0b712d5cb64ba79b2651f8016c1ec0593e7
Valen Rionald <vrionald@gmail.com>
2023-12-21 11:08:21 +0700

    Feat(UserKelompok): Tambah struct untuk user kelompok

---
+++ schemas/ami_user_kelompok.go
@@ -0,0 +1,11 @@
+package model
+
+type AmiUserKelompok struct {
+	IDKelompok   int    `gorm:"primaryKey;column:id_kelompok" json:"-"`
+	NamaKelompok string `gorm:"column:nama_kelompok" json:"nama_kelompok"`
+	KodeUnit     string `gorm:"column:kode_unit" json:"kode_unit"`
+}
+
+func (*AmiUserKelompok) TableName() string {
+	return "ami_user_kelompok"
+}
