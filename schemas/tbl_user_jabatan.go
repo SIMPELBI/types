f25b1221d2fe6463d062084a81648680dc99c7b6
Valen Rionald <vrionald@gmail.com>
2023-12-27 15:29:35 +0700

    Fix(UserJabatan): Perbaikan nama struct

--- schemas/tbl_user_jabatan.go
+++ schemas/tbl_user_jabatan.go
@@ -1,12 +1,12 @@
 package model
 
-type AmiUserJabatan struct {
+type TblUserJabatan struct {
 	ID        int    `gorm:"primaryKey;column:id" json:"-"`
 	Singkatan string `gorm:"column:Singkatan" json:"Singkatan"`
 	Nama      string `gorm:"column:Nama" json:"Nama"`
 	KodeUnit  string `gorm:"column:kode_unit" json:"kode_unit"`
 }
 
-func (m *AmiUserJabatan) TableName() string {
+func (m *TblUserJabatan) TableName() string {
 	return "tbl_user_jabatan"
 }
