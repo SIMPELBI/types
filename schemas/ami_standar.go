5f0e4b46fe5b5ac2cfeaa34deefa72f197f1b956
Valen Rionald <vrionald@gmail.com>
2024-01-04 11:08:22 +0700

    Fix(Standar): Perbaikan struct standar ami

--- schemas/ami_standar.go
+++ schemas/ami_standar.go
@@ -1,22 +1,21 @@
 package model
 
-import "database/sql"
-
 type AmiStandar struct {
-	IDStandar  int            `gorm:"primaryKey;column:id_standar" json:"-"`
-	Standar    sql.NullString `gorm:"column:standar" json:"standar"`
-	UtkPilihan string         `gorm:"column:utk_pilihan" json:"utk_pilihan"`
-	Isi        string         `gorm:"column:isi" json:"isi"`
-	IDSiklus   int            `gorm:"column:id_siklus" json:"id_siklus"`
+	IDStandar   int    `gorm:"primaryKey;column:id_standar" json:"-"`
+	IDIndikator int    `gorm:"column:id_indikator" json:"id_indikator"`
+	Standar     string `gorm:"column:standar" json:"standar"`
+	Isi         string `gorm:"column:isi" json:"isi"`
+	IDProdiUnit int    `gorm:"column:id_prodi_unit" json:"id_prodi_unit"`
 }
 
 type AmiStandarJoin struct {
-	IDStandar  int            `gorm:"primaryKey;column:id_standar" json:"-"`
-	Standar    sql.NullString `gorm:"column:standar" json:"standar"`
-	UtkPilihan string         `gorm:"column:utk_pilihan" json:"utk_pilihan"`
-	Isi        string         `gorm:"column:isi" json:"isi"`
-	IDSiklus   int            `gorm:"column:id_siklus" json:"id_siklus"`
-	Tahun      string         `gorm:"column:tahun" json:"tahun"`
+	IDStandar     int    `gorm:"primaryKey;column:id_standar" json:"-"`
+	IDIndikator   int    `gorm:"column:id_indikator" json:"id_indikator"`
+	NamaIndikator string `gorm:"column:nama_indikator" json:"nama_indikator"`
+	Standar       string `gorm:"column:standar" json:"standar"`
+	Isi           string `gorm:"column:isi" json:"isi"`
+	IDProdiUnit   int    `gorm:"column:id_prodi_unit" json:"id_prodi_unit"`
+	ProdiUnit     string `gorm:"column:prodi_unit" json:"prodi_unit"`
 }
 
 func (m *AmiStandar) TableName() string {
