52b46dda79f2fbd37c5703a855cfda00ca6eb93e
Valen Rionald <vrionald@gmail.com>
2024-01-12 15:44:29 +0700

    Fix(UserLevel): Ganti ke tabel yang ada di amicrot

--- schemas/tbl_user_level.go
+++ schemas/tbl_user_level.go
@@ -1,12 +1,10 @@
 package model
 
-import "database/sql"
-
 type TblUserLevel struct {
-	IDUserLevel int            `gorm:"primaryKey;column:id_user_level" json:"-"`
-	NamaLevel   sql.NullString `gorm:"column:nama_level" json:"nama_level"`
+	IDUserLevel int    `gorm:"primaryKey;column:id_user_level" json:"-"`
+	NamaLevel   string `gorm:"column:nama_level" json:"nama_level"`
 }
 
 func (m *TblUserLevel) TableName() string {
-	return "tbl_user_level"
+	return "ami_user_level"
 }
