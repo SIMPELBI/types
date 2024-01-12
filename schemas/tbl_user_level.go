a0bb7e25ed82ef7b5229edbf41b677f9333103cc
Valen Rionald <vrionald@gmail.com>
2024-01-12 15:46:02 +0700

    Fix(UserLevel): Perbaikan nama struct

--- schemas/tbl_user_level.go
+++ schemas/tbl_user_level.go
@@ -1,10 +1,10 @@
 package model
 
-type TblUserLevel struct {
+type AmiUserLevel struct {
 	IDUserLevel int    `gorm:"primaryKey;column:id_user_level" json:"-"`
 	NamaLevel   string `gorm:"column:nama_level" json:"nama_level"`
 }
 
-func (m *TblUserLevel) TableName() string {
+func (m *AmiUserLevel) TableName() string {
 	return "ami_user_level"
 }
