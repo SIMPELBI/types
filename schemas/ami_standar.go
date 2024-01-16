0d0858e45f0ed2adf4ff7ec0c9d5cb63fffd1e9f
Valen Rionald <vrionald@gmail.com>
2024-01-16 10:50:45 +0700

    Feat(Standar): Tambah field isi indikator

--- schemas/ami_standar.go
+++ schemas/ami_standar.go
@@ -13,6 +13,7 @@ type AmiStandarJoin struct {
 	IDStandar     int    `gorm:"primaryKey;column:id_standar" json:"-"`
 	IDIndikator   int    `gorm:"column:id_indikator" json:"id_indikator"`
 	NamaIndikator string `gorm:"column:nama_indikator" json:"nama_indikator"`
+	IsiIndikator  string `gorm:"column:isi" json:"isi_indikator"`
 	Standar       string `gorm:"column:standar" json:"standar"`
 	Isi           string `gorm:"column:isi" json:"isi"`
 	IDProdiUnit   int    `gorm:"column:id_prodi_unit" json:"id_prodi_unit"`
