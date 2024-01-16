43c249b3a8439e7faab34122f930ff87e3756c81
Valen Rionald <vrionald@gmail.com>
2024-01-16 11:37:59 +0700

    Fix(Standar): Perbaikan nama field isi standar

--- schemas/ami_standar.go
+++ schemas/ami_standar.go
@@ -11,11 +11,11 @@ type AmiStandar struct {
 
 type AmiStandarJoin struct {
 	IDStandar     int    `gorm:"primaryKey;column:id_standar" json:"-"`
+	Standar       string `gorm:"column:standar" json:"standar"`
+	IsiStandar    string `gorm:"column:isi" json:"isi_standar"`
 	IDIndikator   int    `gorm:"column:id_indikator" json:"id_indikator"`
 	NamaIndikator string `gorm:"column:nama_indikator" json:"nama_indikator"`
 	IsiIndikator  string `gorm:"column:isi" json:"isi_indikator"`
-	Standar       string `gorm:"column:standar" json:"standar"`
-	Isi           string `gorm:"column:isi" json:"isi"`
 	IDProdiUnit   int    `gorm:"column:id_prodi_unit" json:"id_prodi_unit"`
 	ProdiUnit     string `gorm:"column:prodi_unit" json:"prodi_unit"`
 	IDSiklus      int    `gorm:"column:id_siklus" json:"id_siklus"`
