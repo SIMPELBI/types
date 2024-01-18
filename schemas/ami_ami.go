2b085fd878d9eac0c8ca9981f4cac9c21958b75d
Valen Rionald <vrionald@gmail.com>
2024-01-18 11:56:29 +0700

    Feat(AMI): Tambah struct untuk laporan AMI

--- schemas/ami_ami.go
+++ schemas/ami_ami.go
@@ -51,38 +51,47 @@ type RekapTemuan struct {
 	Tahun      string `gorm:"column:tahun" json:"tahun"`
 }
 
-type FileLaporanAmi struct {
-	IDAmi          int    `gorm:"column:id_ami" json:"id_ami"`
-	IDFakultas     int    `gorm:"column:id_fakultas" json:"id_fakultas"`
-	IDProdi        int    `gorm:"column:id_prodi" json:"id_prodi"`
-	IDAuditorKetua int    `gorm:"column:id_auditor_ketua" json:"id_auditor_ketua"`
-	IDAnggota1     int    `gorm:"column:id_anggota1" json:"id_anggota1"`
-	IDAnggota2     int    `gorm:"column:id_anggota2" json:"id_anggota2"`
-	IDSiklus       int    `gorm:"column:id_siklus" json:"id_siklus"`
-	Status         string `gorm:"column:status" json:"status"`
-	TglRtm         string `gorm:"column:tgl_rtm" json:"tgl_rtm"`
-	TglSelesai     string `gorm:"column:tgl_selesai" json:"tgl_selesai"`
-	NmAuditorKetua string `gorm:"column:nm_auditor_ketua" json:"nm_auditor_ketua"`
-	NmAuditor1     string `gorm:"column:anggota1" json:"anggota1"`
-	NmAuditor2     string `gorm:"column:anggota2" json:"anggota2"`
-	Fakultas       string `gorm:"column:fakultas" json:"fakultas"`
-	Prodi          string `gorm:"column:prodi" json:"prodi"`
-	Tahun          string `gorm:"column:tahun" json:"tahun"`
-	IDJenjang      int    `gorm:"column:id_jenjang" json:"id_jenjang"`
-	Jenjang        string `gorm:"column:jenjang" json:"jenjang"`
-	IDMekanisme    int    `gorm:"column:id_mekanisme" json:"id_mekanisme"`
-	Question1      string `gorm:"column:question1" json:"question1"`
-	Question2      string `gorm:"column:question2" json:"question2"`
-	Question3      string `gorm:"column:question3" json:"question3"`
-	Question4      string `gorm:"column:question4" json:"question4"`
-	Question5      string `gorm:"column:question5" json:"question5"`
-	Question6      string `gorm:"column:question6" json:"question6"`
-	IDAudit        int    `gorm:"column:id_audit" json:"id_audit"`
-	Uraian         string `gorm:"column:uraian" json:"uraian"`
-	IDKesimpulan   int    `gorm:"column:id_kesimpulan" json:"id_kesimpulan"`
-	CkpLengkap     string `gorm:"column:ckp_lurator" json:"ckp_lurator"`
-	IDKts          int    `gorm:"column:id_kts" json:"id_kts"`
-	Kts            string `gorm:"column:kts" json:"kts"`
+type LaporanAMI struct {
+	IDAmi                   int         `gorm:"primaryKey;column:id_ami" json:"-"`
+	IDFakultas              int         `gorm:"column:id_fakultas" json:"id_fakultas"`
+	Fakultas                string      `gorm:"column:fakultas" json:"fakultas,omitempty"`
+	IDProdiUnit             int         `gorm:"column:id_prodi_unit" json:"id_prodi_unit"`
+	ProdiUnit               string      `gorm:"column:prodi_unit" json:"prodi_unit,omitempty"`
+	NamaKaprodiDir          string      `gorm:"column:nama" json:"nama_kaprodi_dir"`
+	TelponKaprodiDir        string      `gorm:"column:telp" json:"telpon_kaprodi_dir"`
+	IDJenjang               int         `gorm:"column:id_jenjang" json:"id_jenjang"`
+	Jenjang                 string      `gorm:"column:jenjang" json:"jenjang"`
+	IDAuditorKetua          int         `gorm:"column:id_auditor_ketua" json:"id_auditor_ketua"`
+	NmAuditorKetua          string      `gorm:"column:nm_auditor" json:"nm_auditor_ketua,omitempty"`
+	FakultasKetuaAuditor    string      `gorm:"column:fakultas" json:"fakultas_ketua_auditor"`
+	TelponAuditorKetua      string      `gorm:"column:telp" json:"telpon_auditor_ketua"`
+	IDAnggota1              int         `gorm:"column:id_anggota1" json:"id_anggota1"`
+	NmAuditor1              string      `gorm:"column:nm_auditor" json:"nm_auditor_1,omitempty"`
+	FakultasAnggotaAuditor1 string      `gorm:"column:fakultas" json:"fakultas_anggota_auditor_1"`
+	TelponAnggotaAuditor1   string      `gorm:"column:telp" json:"telpon_anggota_auditor_1"`
+	IDAnggota2              int         `gorm:"column:id_anggota2" json:"id_anggota2"`
+	NmAuditor2              string      `gorm:"column:nm_auditor" json:"nm_auditor_2,omitempty"`
+	FakultasAnggotaAuditor2 string      `gorm:"column:fakultas" json:"fakultas_anggota_auditor_2"`
+	TelponAnggotaAuditor2   string      `gorm:"column:telp" json:"telpon_anggota_auditor_2"`
+	IDSiklus                int         `gorm:"column:id_siklus" json:"id_siklus"`
+	Tahun                   int         `gorm:"column:tahun" json:"tahun"`
+	Status                  string      `gorm:"column:status" json:"status"`
+	TglRtm                  carbon.Date `gorm:"column:tgl_rtm" json:"tgl_rtm,omitempty"`
+	TglSelesai              carbon.Date `gorm:"column:tgl_selesai" json:"tgl_selesai,omitempty"`
+	IDStandar               int         `gorm:"column:id_standar" json:"id_standar"`
+	Standar                 string      `gorm:"column:standar" json:"standar,omitempty"`
+	IsiStandar              string      `gorm:"column:isi" json:"isi_standar"`
+	IDIndikator             int         `gorm:"column:id_indikator" json:"id_indikator"`
+	NamaIndikator           string      `gorm:"column:nama_indikator" json:"nama_indikator"`
+	IsiIndikator            string      `gorm:"column:isi" json:"isi_indikator"`
+	Jawaban                 string      `gorm:"column:jawaban" json:"jawaban"`
+	IDKts                   int         `gorm:"column:id_kts" json:"id_kts"`
+	IDAudit                 int         `gorm:"column:id_audit" json:"id_audit"`
+	Kts                     string      `gorm:"column:kts" json:"kts"`
+	Uraian                  string      `gorm:"column:uraian" json:"uraian"`
+	Tindakan                string      `gorm:"column:tindakan" json:"tindakan"`
+	Target                  string      `gorm:"column:target" json:"target"`
+	Email                   string      `gorm:"column:email" json:"email"`
 }
 
 func (m *AmiAmi) TableName() string {
