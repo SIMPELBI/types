f8f5b84bad4c0e611f6506c973f926d62d4540f8
Valen Rionald <vrionald@gmail.com>
2024-01-23 11:25:24 +0700

    Feat(Audit): Tambah field untuk link perbaikan

--- schemas/ami_audit.go
+++ schemas/ami_audit.go
@@ -5,18 +5,19 @@ import (
 )
 
 type AmiAudit struct {
-	IDAudit      int           `gorm:"primaryKey;column:id_audit" json:"-"`
-	IDAmi        int           `gorm:"column:id_ami" json:"id_ami"`
-	IDStandar    int           `gorm:"column:id_standar" json:"id_standar"`
-	IDKts        int           `gorm:"column:id_kts" json:"id_kts"`
-	Uraian       string        `gorm:"column:uraian" json:"uraian"`
-	Tindakan     string        `gorm:"column:tindakan" json:"tindakan"`
-	Target       string        `gorm:"column:target" json:"target"`
-	Status       string        `gorm:"column:status" json:"status"`
-	Tgl          carbon.Carbon `gorm:"column:tgl" json:"tgl"`
-	IDAuditor    int           `gorm:"column:id_auditor" json:"id_auditor"`
-	TglPerbaikan carbon.Carbon `gorm:"column:tgl_perbaikan" json:"tgl_perbaikan"`
-	Jawaban      string        `gorm:"column:jawaban" json:"jawaban"`
+	IDAudit       int           `gorm:"primaryKey;column:id_audit" json:"-"`
+	IDAmi         int           `gorm:"column:id_ami" json:"id_ami"`
+	IDStandar     int           `gorm:"column:id_standar" json:"id_standar"`
+	IDKts         int           `gorm:"column:id_kts" json:"id_kts"`
+	Uraian        string        `gorm:"column:uraian" json:"uraian"`
+	Tindakan      string        `gorm:"column:tindakan" json:"tindakan"`
+	Target        string        `gorm:"column:target" json:"target"`
+	Status        string        `gorm:"column:status" json:"status"`
+	Tgl           carbon.Carbon `gorm:"column:tgl" json:"tgl"`
+	IDAuditor     int           `gorm:"column:id_auditor" json:"id_auditor"`
+	TglPerbaikan  carbon.Carbon `gorm:"column:tgl_perbaikan" json:"tgl_perbaikan"`
+	Jawaban       string        `gorm:"column:jawaban" json:"jawaban"`
+	LinkPerbaikan string        `gorm:"column:link_perbaikan" json:"link_perbaikan"`
 }
 
 type AmiAuditJoin struct {
