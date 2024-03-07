28515279030c6f888a7e9cb7d60c08c1b70544c3
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-03-07 14:46:00 +0700

    create struct tbl publikasi for webometrics

---
+++ schemas/tbl_publikasi.go
@@ -0,0 +1,18 @@
+package model
+
+type TblPublikasi struct {
+	Id              int    `gorm:"primaryKey;column:id" json:"-"`
+	NamaDosen       int    `gorm:"column:nama_dosen" json:"nama_dosen"`
+	JudulPaper      string `gorm:"column:judul_paper" json:"judul_paper"`
+	TahunTerbit     string `gorm:"column:tahun_terbit" json:"tahun_terbit"`
+	NamaPublikasi   string `gorm:"column:nama_publikasi" json:"nama_publikasi"`
+	JenisPublikasi  string `gorm:"column:jenis_publikasi" json:"jenis_publikasi"`
+	Ranking         string `gorm:"column:ranking" json:"ranking"`
+	JenisPenelitian string `gorm:"jenis_penelitian" json:"jenis_penelitian"`
+	Autor           string `gorm:"autor" json:"autor"`
+	JumlahKutipan   int    `gorm:"jumlah_kutipan" json:"jumlah_kutipan"`
+}
+
+func (m *TblPublikasi) TableName() string {
+	return "publikasi"
+}
