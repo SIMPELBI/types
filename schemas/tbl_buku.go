c212c5611f3991159e381381206eaa6eb2e827f8
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-03-07 14:45:51 +0700

    create struct tbl buku for webometrics

---
+++ schemas/tbl_buku.go
@@ -0,0 +1,18 @@
+package model
+
+type TblBuku struct {
+	Id              int    `gorm:"primaryKey;column:id" json:"-"`
+	NamaDosen       int    `gorm:"column:nama_dosen" json:"nama_dosen"`
+	JudulBuku       string `gorm:"column:judul_buku" json:"judul_buku"`
+	TahunTerbit     string `gorm:"column:tahun_terbit" json:"tahun_terbit"`
+	Penerbit        string `gorm:"column:penerbit" json:"penerbit"`
+	JenisPublikasi  string `gorm:"column:jenis_publikasi" json:"jenis_publikasi"`
+	Ranking         string `gorm:"column:ranking" json:"ranking"`
+	JenisPenelitian string `gorm:"jenis_penelitian" json:"jenis_penelitian"`
+	Autor           string `gorm:"autor" json:"autor"`
+	JumlahKutipan   int    `gorm:"jumlah_kutipan" json:"jumlah_kutipan"`
+}
+
+func (m *TblBuku) TableName() string {
+	return "buku"
+}
