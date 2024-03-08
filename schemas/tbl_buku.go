8f63de3207e6bd1977e2ed0c0df4c59d32ef16b5
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-03-08 13:29:47 +0700

    add column kode_prodi in struct TblPublikasi & TblBuku

--- schemas/tbl_buku.go
+++ schemas/tbl_buku.go
@@ -3,6 +3,7 @@ package model
 type TblBuku struct {
 	Id              int    `gorm:"primaryKey;column:id" json:"-"`
 	NamaDosen       string `gorm:"column:nama_dosen" json:"nama_dosen"`
+	KodeProdi       int    `gorm:"column:kode_prodi" json:"kode_prodi"`
 	JudulBuku       string `gorm:"column:judul_buku" json:"judul_buku"`
 	TahunTerbit     string `gorm:"column:tahun_terbit" json:"tahun_terbit"`
 	Penerbit        string `gorm:"column:penerbit" json:"penerbit"`
--- schemas/tbl_publikasi.go
+++ schemas/tbl_publikasi.go
@@ -3,6 +3,7 @@ package model
 type TblPublikasi struct {
 	Id              int    `gorm:"primaryKey;column:id" json:"-"`
 	NamaDosen       string `gorm:"column:nama_dosen" json:"nama_dosen"`
+	KodeProdi       int    `gorm:"column:kode_prodi" json:"kode_prodi"`
 	JudulPaper      string `gorm:"column:judul_paper" json:"judul_paper"`
 	TahunTerbit     string `gorm:"column:tahun_terbit" json:"tahun_terbit"`
 	NamaPublikasi   string `gorm:"column:nama_publikasi" json:"nama_publikasi"`
